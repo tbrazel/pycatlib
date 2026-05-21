65268d34d88c36e27f6143510bb699d941eae997
Ben Spitz <diracdeltafunk@users.noreply.github.com>
2026-05-21 12:42:44 -0400

    rewrite square_bracket without using strings

--- src/pycatlib/category.py
+++ src/pycatlib/category.py
@@ -504,36 +504,20 @@ def from_minion_matrix(mat: list, objs=None) -> Category:
 def square_bracket(n: int) -> Category:
     objects = list(range(n + 1))
 
-    morphisms = []
-    for i in range(n + 1):
-        for j in range(i, n + 1):
-            morphisms.append(str([i, j]))
+    morphisms = [(i, j) for i in range(n + 1) for j in range(i, n + 1)]
     # Make domain and codomain
-    domain = {}
-    codomain = {}
-    for f in morphisms:
-        flist = f.strip("[]").split(",")
-        flist = [int(x) for x in flist]
-        domain[f] = flist[0]
-        codomain[f] = flist[1]
+    domain = {f: f[0] for f in morphisms}
+    codomain = {f: f[1] for f in morphisms}
 
     # Make identities
-    id_dictionary = {}
-    for i in range(n + 1):
-        id_dictionary[i] = str([i, i])
+    id_dictionary = {i: (i, i) for i in range(n + 1)}
 
     # Make composition
     composition = {}
-    for f in morphisms:
-        for g in morphisms:
-            # Remember these are strings so we turn them back into lists quick
-            flist = f.strip("[]").split(",")
-            flist = [int(x) for x in flist]
-            glist = g.strip("[]").split(",")
-            glist = [int(x) for x in glist]
-
-            if glist[1] == flist[0]:
-                composition[(f, g)] = str([glist[0], flist[1]])
+    for i in range(n + 1):
+        for j in range(i, n + 1):
+            for k in range(j, n + 1):
+                composition[((i, j), (j, k))] = (i, k)
 
     return Category(
         objects=objects,
